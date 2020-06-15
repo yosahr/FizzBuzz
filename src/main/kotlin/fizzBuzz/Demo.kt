df9dbdabc2645b1287e2aa5f97c564e20639ac82
Yohanan Romem <john.romem@dell.com>
2020-06-15 18:42:07 +0100

    Green

--- src/main/kotlin/fizzBuzz/Demo.kt
+++ src/main/kotlin/fizzBuzz/Demo.kt
@@ -3,6 +3,7 @@ package fizzBuzz
 class Demo {
     fun fizzBuzz(number: Int): String =
             when {
+                number % 15 == 0 -> "FizzBuzz"
                 number % 3 == 0 -> "Fizz"
                 number % 5 == 0 -> "Buzz"
                 else -> number.toString()
