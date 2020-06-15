36e6dbac3c013e0dc710340831f68bce17d05942
Yohanan Romem <john.romem@dell.com>
2020-06-15 18:55:33 +0100

    Refactor

--- src/main/kotlin/fizzBuzz/Demo.kt
+++ src/main/kotlin/fizzBuzz/Demo.kt
@@ -1,12 +1,14 @@
 package fizzBuzz
 
 class Demo {
-    fun fizzBuzz(number: Int): String =
-            when {
-                number % 15 == 0 -> "FizzBuzz"
-                number % 3 == 0 -> "Fizz"
-                number % 5 == 0 -> "Buzz"
-                else -> number.toString()
-            }
 
+    companion object {
+        fun fizzBuzz(number: Int): String =
+                when {
+                    number % 15 == 0 -> "FizzBuzz"
+                    number % 3 == 0 -> "Fizz"
+                    number % 5 == 0 -> "Buzz"
+                    else -> number.toString()
+                }
+    }
 }
--- src/test/kotlin/fizzBuzz/DemoTest.kt
+++ src/test/kotlin/fizzBuzz/DemoTest.kt
@@ -1,29 +1,28 @@
 package fizzBuzz
 
+import fizzBuzz.Demo.Companion.fizzBuzz
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class DemoTest {
 
-    private val demo = Demo()
-
     @Test
     fun `should be Fizz when number is 3`() {
-        assertThat(demo.fizzBuzz(3)).isEqualTo("Fizz")
+        assertThat(fizzBuzz(3)).isEqualTo("Fizz")
     }
 
     @Test
     fun `should be 4 when number is 4`() {
-        assertThat(demo.fizzBuzz(4)).isEqualTo("4")
+        assertThat(fizzBuzz(4)).isEqualTo("4")
     }
 
     @Test
     fun `should be Buzz when number is 5`() {
-        assertThat(demo.fizzBuzz(5)).isEqualTo("Buzz")
+        assertThat(fizzBuzz(5)).isEqualTo("Buzz")
     }
 
     @Test
     fun `should be FizzBuzz when number is 15`() {
-        assertThat(demo.fizzBuzz(15)).isEqualTo("FizzBuzz")
+        assertThat(fizzBuzz(15)).isEqualTo("FizzBuzz")
     }
 }
