97f4f27474f2c855f2ac17471d2b7da2b81d258c
Yohanan Romem <john.romem@dell.com>
2020-06-15 18:28:06 +0100

    compiling, failing test

---
+++ src/main/kotlin/fizzBuzz/Demo.kt
@@ -0,0 +1,8 @@
+package fizzBuzz
+
+class Demo {
+    fun fizzBuzz(number: Int): String {
+        return ""
+    }
+
+}
--- src/test/kotlin/fizzBuzz/DemoTest.kt
+++ src/test/kotlin/fizzBuzz/DemoTest.kt
@@ -1,9 +1,13 @@
 package fizzBuzz
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
 class DemoTest {
 
     @Test
     fun `should return Fizz when number is 3`() {
+        val demo = Demo()
         assertThat(demo.fizzBuzz(3)).isEqualTo("Fizz")
     }
 }
