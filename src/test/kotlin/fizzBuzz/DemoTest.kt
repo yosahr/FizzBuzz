dd41a5ce7d6c375369a861b7a4b8a5c0a1947b51
Yohanan Romem <john.romem@dell.com>
2020-06-15 18:35:00 +0100

    Refactor

--- src/test/kotlin/fizzBuzz/DemoTest.kt
+++ src/test/kotlin/fizzBuzz/DemoTest.kt
@@ -5,15 +5,15 @@ import org.junit.Test
 
 class DemoTest {
 
+    private val demo = Demo()
+
     @Test
     fun `should be Fizz when number is 3`() {
-        val demo = Demo()
         assertThat(demo.fizzBuzz(3)).isEqualTo("Fizz")
     }
 
     @Test
     fun `should be 4 when number is 4`() {
-        val demo = Demo()
         assertThat(demo.fizzBuzz(4)).isEqualTo("4")
     }
 }
