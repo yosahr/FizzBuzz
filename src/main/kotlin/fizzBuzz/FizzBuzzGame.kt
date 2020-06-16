95561d80283acd9561770bd840e6e059b5b7180c
Yohanan Romem <john.romem@dell.com>
2020-06-16 16:07:02 +0100

    Rename Class from Demo to FizzBuzzGame

--- src/main/kotlin/fizzBuzz/FizzBuzzGame.kt
+++ src/main/kotlin/fizzBuzz/FizzBuzzGame.kt
@@ -1,9 +1,9 @@
 package fizzBuzz
 
-class Demo {
+class FizzBuzzGame {
 
     companion object {
-        fun fizzBuzz(number: Int): String =
+        fun play(number: Int): String =
                 when {
                     number % 15 == 0 -> "FizzBuzz"
                     number % 3 == 0 -> "Fizz"
